c723c399c808c2d779ac9db794c60a5a3caffec3
Dylan920424 <dylan920424@gmail.com>
2023-11-21 15:43:14 -0600

    part 1

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-import sys, json
+import sys, json, time
 inFile = sys.argv[1]
 outFile = sys.argv[2]
 
@@ -8,6 +8,7 @@ with open(inFile,'r') as i:
     lines = i.readlines()
     for domain in lines:
         results[domain] = {}
+        results[domain]["scan_time"] = time.time()
 
 with open(outFile , "w") as f :
     json.dump(results, f, sort_keys=True, indent =4)

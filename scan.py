a842aace71ec075e6009c798a8024aecf6090e75
jkv64 <jackson.kenneth.vogel@gmail.com>
2023-11-27 17:42:10 -0600

    agh

--- scan.py
+++ scan.py
@@ -37,13 +37,62 @@ with open(inFile,'r') as i:
             results[domain]["http_server"] = server[0][1:-1]
         else:
             results[domain]["http_server"] = None
-        # r = requests.get("https://"+domain)
-        # if "Server" in r.headers:
-        #     results[domain]["http_server"] = r.headers["Server"]
-        # else:
-        #     results[domain]["http_server"] = None
-        
-        
 
-with open(outFile , "w") as f :
-    json.dump(results, f, sort_keys=True, indent =4)
+        url = "http://" + domain
+        status = re.findall(r"(?<=HTTP\/).* [0-9]{3}", result)[0].split(" ")[1]
+        print(status)
+        if int(status) >= 500:
+            results[domain]["insecure_http"] = False
+            results[domain]["redirect_to_https"] = False
+        else:
+            results[domain]["insecure_http"] = True
+            redirects = 0
+            while (300 <= int(status) < 400) and redirects < 10:
+                # use regex to find the redirection url
+                url = re.findall(r'(?<=[L|l]ocation: ).*', result)[0][:-1]
+                if url.split(":")[0] == "https":
+                    results[domain]["redirect_to_https"] = True
+                    break
+                try:
+                    result = subprocess.check_output(["curl", "-v", url], timeout = 2, stderr=subprocess.STDOUT).decode("utf-8")
+                except:
+                    result = ""
+                    print("timed out")
+                    break
+                redirects += 1
+                status = re.findall(r"(?<=HTTP\/).* [0-9]{3}", result)[0].split(" ")[1]
+                print(status)
+            if "redirect_to_https" not in results[domain]:
+                results[domain]["redirect_to_https"] = False
+
+        curl_process = subprocess.Popen(["curl", "-s", "-D-", url], stdout = subprocess.PIPE)
+        grep_process = subprocess.Popen(["grep", "-i", "Strict-Transport-Security"], stdin=curl_process.stdout, stdout = subprocess.PIPE)
+        hsts, error = grep_process.communicate()
+        results[domain]["hsts"] = bool(hsts)
+
+        try:
+            nmap_out = subprocess.check_output(["nmap", "--script", "ssl-enum-ciphers", "-p", "443", domain], timeout = 2, stderr=subprocess.STDOUT).decode("utf-8")
+        except subprocess.TimeoutExpired:
+            print("timed out")
+        tls = [d[0] for d in re.findall(r"((TLS|SSL)v.*(?=:))", nmap_out)]
+        try:
+            openssl_out = subprocess.check_output(["openssl", "s_client", "-connect", domain+":443"], input=b'', timeout = 2, stderr=subprocess.STDOUT).decode("utf-8")
+            root_ca = re.findall(r"O = (.*), .*(?=Root CA)", openssl_out)[0].split(",")[0]
+        except Exception as e:
+            root_ca = None
+            if e == subprocess.TimeoutExpired:
+                print("timed out")
+        
+        results[domain]["tls_versions"] = tls
+        results[domain]["root_ca"] = root_ca
+
+        addresses = []
+        for addr in results[domain]["ipv4_addresses"]:
+            try:
+                dig_out = subprocess.check_output(["dig", "+answer", "-x", addr], timeout = 2, stderr=subprocess.STDOUT).decode("utf-8")
+                # regex for PTR
+                addresses += dig_out
+            except Exception as e:
+                if e == subprocess.TimeoutExpired:
+                    print("timed out")
+        results[domain]["rdns_names"] = addresses
